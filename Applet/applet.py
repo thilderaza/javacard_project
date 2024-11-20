910d9625c9b548822ccedae0e34c6844af65a85d
DimitriMo <dimitri.morelle@gmail.com>
2024-11-20 14:01:11 +0100

    init function

--- Applet/applet.py
+++ Applet/applet.py
@@ -1,9 +1,16 @@
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+import os
 
 class SmartCardApplet:
     def __init__(self, pin="1234"):
-
+# Stocke le PIN et génère une paire de clés RSA
+        self.pin = pin
+        self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=512)
+        self.public_key = self.private_key.public_key()
+        print("Smart card initialized with a PIN and RSA key pair.")
 
     def verify_pin(self, pin_input):
 
