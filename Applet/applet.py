7173bfb8c1160d2cbd8ef74d38b47f11187f0e19
DimitriMo <dimitri.morelle@gmail.com>
2024-11-20 14:06:07 +0100

    sign function

--- Applet/applet.py
+++ Applet/applet.py
@@ -4,23 +4,36 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 import os
 
+
 class SmartCardApplet:
+
+    # Stores PIN and generates RSA key pair
     def __init__(self, pin="1234"):
-# Stocke le PIN et génère une paire de clés RSA
         self.pin = pin
         self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=512)
         self.public_key = self.private_key.public_key()
         print("Smart card initialized with a PIN and RSA key pair.")
 
+    # Verifies if the pIN is correct
     def verify_pin(self, pin_input):
 
-
+    # Signs data with private key
     def sign_data(self, data):
+        
+        if not isinstance(data, bytes):
+            raise ValueError("Data must be in bytes format.")
+        signature = self.private_key.sign(
+            data,
+            padding.PKCS1v15(),
+            hashes.SHA256()
+        )
+        return signature
 
-
+    # Returns the public key in PEM format
     def get_public_key(self):
         
-
+    
+    # Manages the reconstitution of data sent via multiple APDUs
     def handle_multi_apdu(self, apdu_chunks):
 
 
