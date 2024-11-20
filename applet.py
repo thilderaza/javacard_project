42af391355ce8cfd5293b6949bcdb8b8672be162
DimitriMo <dimitri.morelle@gmail.com>
2024-11-20 13:43:10 +0100

    Methods

---
+++ applet.py
@@ -0,0 +1,19 @@
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+
+class SmartCardApplet:
+    def __init__(self, pin="1234"):
+
+
+    def verify_pin(self, pin_input):
+
+
+    def sign_data(self, data):
+
+
+    def get_public_key(self):
+        
+
+    def handle_multi_apdu(self, apdu_chunks):
+
+
